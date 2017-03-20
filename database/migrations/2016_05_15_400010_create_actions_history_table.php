c693149a4f917b0c108b74ad0303dcc85bace711
aocneanu <aocneanu@gmail.com>
2017-03-20 08:48:56 +0000

    Apply fixes from StyleCI

--- database/migrations/2016_05_15_400010_create_actions_history_table.php
+++ database/migrations/2016_05_15_400010_create_actions_history_table.php
@@ -3,33 +3,32 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateActionsHistoryTable extends Migration {
+class CreateActionsHistoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('actions_history', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->index('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('restrict')->onDelete('restrict');
+            $table->string('route');
+            $table->string('action');
+            $table->timestamps();
+        });
+    }
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('actions_history', function(Blueprint $table) {
-
-			$table->increments('id');
-			$table->integer('user_id')->unsigned()->index('user_id');
-			$table->foreign('user_id')->references('id')->on('users')->onUpdate('restrict')->onDelete('restrict');
-			$table->string('route');
-			$table->string('action');
-			$table->timestamps();
-		});
-	}
-
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('actions_history');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('actions_history');
+    }
 }
