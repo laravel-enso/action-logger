ee66471dca364ee616a07c317b3d08df2964d796
mishu <mishu@xtelecom.ro>
2017-04-02 01:25:08 +0300

    fix migration

--- src/database/migrations/2017_01_01_130000_create_action_histories_table.php
+++ src/database/migrations/2017_01_01_130000_create_action_histories_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateActionHistoryTable extends Migration
+class CreateActionHistoriesTable extends Migration
 {
     /**
      * Run the migrations.
