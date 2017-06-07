3e1f7ddcc6cddb52df3c4f89b7f0512e605aed47
mishu <mishu@xtelecom.ro>
2017-06-07 12:11:31 +0300

    removed useless comments

--- src/ActionLoggerServiceProvider.php
+++ src/ActionLoggerServiceProvider.php
@@ -7,22 +7,12 @@ use LaravelEnso\ActionLogger\app\Http\Middleware\ActionLogger;
 
 class ActionLoggerServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap the application services.
-     *
-     * @return void
-     */
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
         $this->app['router']->aliasMiddleware('action-logger', ActionLogger::class);
     }
 
-    /**
-     * Register the application services.
-     *
-     * @return void
-     */
     public function register()
     {
         //
