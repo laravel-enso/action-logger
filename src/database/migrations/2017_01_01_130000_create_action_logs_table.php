3e335a9bf04ab67e1beabb242bd1ab59a499841d
mishu <mishu@xtelecom.ro>
2018-06-06 08:51:31 +0300

    updates composer.json

--- src/database/migrations/2017_01_01_130000_create_action_logs_table.php
+++ src/database/migrations/2017_01_01_130000_create_action_logs_table.php
@@ -9,12 +9,15 @@ class CreateActionLogsTable extends Migration
     {
         Schema::create('action_logs', function (Blueprint $table) {
             $table->increments('id');
+
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users')
                 ->onUpdate('cascade')->onDelete('cascade');
+
             $table->string('url');
             $table->string('route')->index();
             $table->string('method');
+
             $table->timestamps();
         });
     }
