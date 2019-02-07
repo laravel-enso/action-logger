d26e79e0d88456b9f3546baba77caff4ad08254c
mishu <mishu@xtelecom.ro>
2019-02-07 15:31:36 +0200

    removes connection trait

--- src/app/Models/ActionLog.php
+++ src/app/Models/ActionLog.php
@@ -5,22 +5,10 @@ namespace LaravelEnso\ActionLogger\app\Models;
 use LaravelEnso\Core\app\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use LaravelEnso\PermissionManager\app\Models\Permission;
-use LaravelEnso\Multitenancy\app\Traits\SystemConnection;
 
 class ActionLog extends Model
 {
-    use SystemConnection;
-
     protected $fillable = ['user_id', 'url', 'route', 'method'];
-
-    public function __construct($attributes = [])
-    {
-        parent::__construct($attributes);
-
-        $this->connection = config('app.env') === 'testing'
-            ? 'sqlite'
-            : 'system';
-    }
 
     public function user()
     {
