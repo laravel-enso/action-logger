0352bb9b46457388269ceb2c73da849233a8b657
Vlad Chiovianu <vlad.chiovianu@earthlink.ro>
2024-05-02 13:33:38 +0300

    missing request user fix attempt

--- src/AppServiceProvider.php
+++ src/AppServiceProvider.php
@@ -7,6 +7,11 @@ use LaravelEnso\ActionLogger\Http\Middleware\ActionLogger;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public function register(): void
+    {
+        $this->app->singleton(ActionLogger::class);
+    }
+
     public function boot(): void
     {
         $this->app['router']->aliasMiddleware('action-logger', ActionLogger::class);
